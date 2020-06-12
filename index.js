04482b8820a2ea7fb0b3792e8438baa94032f2a8
basalcode <basalcode@gmail.com>
2020-06-12 23:22:18 +0900

    add: Implement 'Post Editor' can write post 1
    What's New?
    * New Featrues
     - 'Post Editor' can now send information about post to server.
    
    * Installed Package
     - 'body-parser' package has been installed.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+/* body-parser */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/post', express.static('./src'));
 
@@ -17,6 +22,12 @@ app.get('/', function (req, res) {
             }
         })
     }
+})
+
+app.post('/writePost', function (req, res) {
+    res.send({
+        result: req.body
+    });
 })
 
 app.listen(3000);
--- src/postEditor.js
+++ src/postEditor.js
@@ -4,4 +4,29 @@ window.onload = function() {
     let title = document.querySelector('#title');
     let author = document.querySelector('#author');
     let description = document.querySelector('#description');
+    let submit = document.querySelector('#submit');
+
+    submit.addEventListener('click', function(event) {
+        let postEditorObj = {
+            title: title.value,
+            author: author.value,
+            description: description.value
+        }
+
+        console.log(JSON.stringify(postEditorObj));
+
+        fetch('/writePost', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postEditorObj)
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(parsed) {
+            console.log(parsed);
+        })
+    })
 }
