0ca544e13f75bc2c02ceabfcf3d2f22a79a65599
basalcode <basalcode@gmail.com>
2020-07-25 23:38:37 +0900

    add: Append anchor tag on category list elements
    What's New?
    * New Features
     - Anchor tags have been applied at category elements on 'Blog Main'
       page, and they connect to separate 'Category' page.

--- src/client/blogMain.js
+++ src/client/blogMain.js
@@ -1,17 +1,17 @@
 import sections from './common/categoryList.js';
 
-window.onload = function() {
+window.onload = function () {
     console.log(`[Open] 'blogMain.js' has been opend.`);
 
     let writePost = document.querySelector('#write-post');
     let readPostList = document.querySelector('#read-post-list');
-    
-    writePost.addEventListener('click', function(event) {
-        window.location.href='/source/postEditor.html?mode=write';
+
+    writePost.addEventListener('click', function (event) {
+        window.location.href = '/source/postEditor.html?mode=write';
     });
 
-    readPostList.addEventListener('click', function(event) {
-        window.location.href='/source/category.html';
+    readPostList.addEventListener('click', function (event) {
+        window.location.href = '/source/category.html';
     });
 
     loadCategoryList();
@@ -21,5 +21,27 @@ async function loadCategoryList() {
     let categoryList = document.querySelector('#category-list');
 
     let loadedSection = await sections;
+    let elements = loadedSection.elements;
     categoryList.appendChild(loadedSection.root);
+
+    appendAnchorElement(elements);
+
+    function appendAnchorElement(elements) {
+        let cateogryLength = elements.category.length;
+        let categorySelfs = elements.categorySelf;
+        let categoryTitles = elements.categoryTitle;
+
+        for (let i = 0; i < cateogryLength; i++) {
+            let categorySelf = categorySelfs[i];
+            let anchorTag = document.createElement('a');
+            let categoryTitle = categoryTitles[i];
+
+            let categoryName = categoryTitle.innerText;
+            anchorTag.href = `/source/category.html?page=${categoryName}`;
+            anchorTag.id = '.category-anchor';
+
+            categorySelf.appendChild(anchorTag);
+            anchorTag.appendChild(categoryTitle);
+        }
+    }
 }
