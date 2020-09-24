42e30e8b954fe858426364194f8e101aa1dfab3e
basalcode <basalcode@gmail.com>
2020-09-24 21:41:27 +0900

    add: Append caret animation on text display
    What's New?
    * New Feature
     - Caret animation has been appended on text display on 'Blog Lobby
       Intro'.
    
    * Refactoring
     - 'Blog Lobby Intro' component has been refactored.

--- src/client/component/page/Blog/Lobby/BlogLobbyIntro.js
+++ src/client/component/page/Blog/Lobby/BlogLobbyIntro.js
@@ -8,71 +8,34 @@ import links from 'asset/img/links/blogLobby';
 import shuffle from 'lib/array/shuffle';
 
 const BlogLobbyIntro = () => {
-    /* const [blogLobbyClassName, setBlogLobbyClassName] = useState('BlogLobbyIntro__animation--before');
-    const [animationFinished, setAnimationFinished] = useState(false);
-
-    const [titleContent, setTitleContent] = useState('');
-    const [textIndex, setTextIndex] = useState(0);
-    const [titleCaret, setTitleCaret] = useState(true);
-    const [clearCaret, setClearCaret] = useState(false);
-    const [titleClassName, setTitleClassName] = useState('BlogLobbyIntro__title--before');
-
-    const [greetingClassName, setGreetingClassName] = useState('BlogLobbyIntro__greeting--before');
-
-    const [downArrowClassName, setDownArrowClassName] = useState('BlogLobbyIntro__down-arrow--before');
-
-    const title = 'Basal Code Studio';
-
-    useEffect(() => {
-        const interval = 1500;
-        setTimeout(() => {
-            setBlogLobbyClassName('BlogLobbyIntro__animation--after');
-            setAnimationFinished(true);
-        }, interval);
-    }, []);
-
-    useEffect(() => {
-        if (animationFinished) {
-            setTextIndex(textIndex + 1);
-            if (textIndex === title.length) {
-                const animationFinishInterval = 1500;
-
-                setTimeout(() => {
-                    setClearCaret(true);
-                    setTitleClassName("BlogLobbyIntro__title--after");
-                    setGreetingClassName("BlogLobbyIntro__greeting--after");
-                }, animationFinishInterval);
-            }
-            const longInterval = 500;
-            const shortInterval = 100;
-            let interval = (title[textIndex] === ' ') ? longInterval : shortInterval;
-            if (textIndex === 0) { interval = 1500; }
-
-            setTimeout(() => {
-                const newText = titleContent + title.charAt(textIndex);
-                setTitleContent(newText);
-            }, interval);
-        }
-    }, [animationFinished, titleContent]);
-
-    useEffect(() => {
-        if (!clearCaret) {
-            const caretInterval = 500;
-            setTimeout(() => {
-                titleCaret ? setTitleCaret(false) : setTitleCaret(true);
-            }, caretInterval);
-        }
-    }, [titleCaret]);
-
-    const greetingText = 'Welcome! :)';
-
-    useEffect(() => {
-        if (clearCaret) {
-            setDownArrowClassName('BlogLobbyIntro__down-arrow--after');
-        }
-    }, [clearCaret]); */
-
     const [pictureLink, setPictureLink] = useState('');
+    const [textAnimation, setTextAnimation] = useState({
+        display: [],
+        charIndex: 0,
+        animationIndex: 0,
+        isCleared: false
+    })
+    const [caretOn, setCaretOn] = useState('_');
+
+    const textData = useMemo(() => {
+        const titleText = 'BasalCodeStudio';
+        const animationChars = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '=',
+            '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'
+        ];
+        const shuffledChars = shuffle(animationChars);
+        const maxRepeat = 10;
+        return {
+            titleText: titleText,
+            animationChars: animationChars,
+            shuffledChars: shuffledChars,
+            maxRepeat: maxRepeat
+        };
+    }, [textAnimation]);
 
     useEffect(() => {
         const changeInterval = 10000;
@@ -96,58 +59,52 @@ const BlogLobbyIntro = () => {
         }, changeInterval);
     }, []);
 
-    const [textAnimation, setTextAnimation] = useState({
-        display: [],
-        charIndex: 0,
-        animationIndex: 0,
-        isCleared: false
-    })
-
-    const titleText = 'BasalCodeStudio';
-
-    const maxRepeat = 10;
-    const animationChars = [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '=',
-        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'
-    ];
-    const shuffledChars = useMemo(() => shuffle(animationChars).slice(0, maxRepeat), []);
-    
     useEffect(() => {
         const animationInterval = 100;
-        if (!textAnimation.isCleared) {
-            setTimeout(() => {
-                if (titleText[textAnimation.charIndex] === shuffledChars[textAnimation.animationIndex] ||
-                    textAnimation.animationIndex >= maxRepeat) {
-                    const tempArray = textAnimation.display.slice();
-                    tempArray[textAnimation.charIndex] = titleText[textAnimation.charIndex];
+
+        const isCleared = textAnimation.isCleared;
+        if (!isCleared) {
+            window.setTimeout(() => {
+                const display = textAnimation.display;
+                const charIndex = textAnimation.charIndex;
+                const animationIndex = textAnimation.animationIndex;
+                const titleText = textData.titleText;
+                const shuffledChars = textData.shuffledChars;
+                const maxRepeat = textData.maxRepeat;
+
+                if (/* titleText[charIndex] === shuffledChars[animationIndex] || */
+                    animationIndex >= maxRepeat) {
+                    const tempArray = display.slice();
+                    tempArray[charIndex] = titleText[charIndex];
 
                     setTextAnimation(previous => ({
-                        ...previous, 
+                        ...previous,
                         display: tempArray,
-                        charIndex: textAnimation.charIndex + 1,
+                        charIndex: charIndex + 1,
                         animationIndex: 0,
-                        isCleared: textAnimation.charIndex + 1 >= titleText.length ?
+                        isCleared: charIndex + 1 >= titleText.length ?
                             true : false
                     }));
-                    console.log(textAnimation);
                     return;
                 }
 
-                const tempArray = textAnimation.display.slice();
-                tempArray[textAnimation.charIndex] = shuffledChars[textAnimation.animationIndex];
+                const tempArray = display.slice();
+                tempArray[charIndex] = shuffledChars[animationIndex];
                 setTextAnimation(previous => ({
-                    ...previous, 
+                    ...previous,
                     display: tempArray,
-                    animationIndex: textAnimation.animationIndex + 1,
+                    animationIndex: animationIndex + 1,
                 }));
-                console.log(textAnimation);
             }, animationInterval);
         }
     }, [textAnimation]);
+
+    useEffect(() => {
+        const caretInterval = 500;
+        window.setTimeout(() => {
+            caretOn === '_' ? setCaretOn('') : setCaretOn('_');
+        }, caretInterval)
+    }, [caretOn]);
 
     return (
         <section className="BlogLobbyIntro">
@@ -165,9 +122,10 @@ const BlogLobbyIntro = () => {
                                 {element}
                             </span>
                         )}
-
+                        <span className="BlogLobbyIntro__greeting-caret">
+                            {caretOn}
+                        </span>
                     </div>
-                    <span className="BlogLobbyIntro__greeting-caret"></span>
                 </div>
             </section>
         </section>
