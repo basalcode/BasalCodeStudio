add03fb4efec12f2eff75696ece5aa3d56c334d7
basalcode <basalcode@gmail.com>
2020-09-29 23:43:19 +0900

    finish: Release version and bump to v0.2
    What's New?
    * Raise Version
     - BasalCodeStudio's version has been increased to v0.2.
     - BasalCodeStudio has been built and deployed in production mode.
     - Package.json has been organized.

--- src/server/index.js
+++ src/server/index.js
@@ -21,8 +21,8 @@ app.use(session);
 // To change developmentMode as false,
 // the "start" value on package.json should be modified 
 // as "export PORT=3001 && react-scripts start"
-const developmentMode = true;
-if (developmentMode) {
+// const developmentMode = true;
+// if (developmentMode) {
     //Client Side Rendering
     app.use('/request/:dbType/:inputType/:contentType', requestProcessor);
     app.use('/auth/:inputType/:session', sessionProcessor);
@@ -30,24 +30,7 @@ if (developmentMode) {
     app.use(errorHandler.error404);
     app.use(errorHandler.error500);
 
-    app.listen(3001);
-} else {
-    //Server Side Rendering
-    // After Apply Redux
-    app.get('/*', (req, res) => {
-        res.sendfile(path.join(__dirname, '../index.html'));
-    })
-
-    // Before Apply Redux
-    /* app.use('/', express.static(path.resolve(__dirname, '../../build')));
-    app.get('*', (req, res, next) => {
-        if (req.path.split('/')[1] === 'static') {
-            return next();
-        }
-        console.log(__dirname);
-
-        res.sendFile(path.resolve(__dirname, '../../build/index.html'));
-    }); */
-
-    app.listen(3000);
-}
+    app.listen(3030);
+// } else {
+    
+// }
